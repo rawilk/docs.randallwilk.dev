3ab70a3f25106ad385159671996d684aedc1a7af
Randall Wilk <wilk.randall@gmail.com>
2020-08-13 12:11:11 -0500

    Cleanup routes

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\DocsController;
 use Illuminate\Support\Facades\Route;
 
 // Package redirects to their "master" branches
-Route::redirect('access', 'access/v1/introduction');
+Route::redirect('laravel-printing', 'laravel-printing/v1/introduction');
 
 Route::get('{package}/{version}/{doc}', DocsController::class)
     ->where('doc', '.*')
