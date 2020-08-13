4e5e3b890af3c3cc52ffac5c55fab6d56e2eb10b
Randall Wilk <wilk.randall@gmail.com>
2020-08-13 12:19:03 -0500

    Compress assets, add canonical meta tag

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -12,9 +12,10 @@
         <title>{{ $title }}</title>
         @stack('meta')
         <link rel="home" href="{{ config('app.url') }}">
-
+        <link rel="canonical" href="{{ url(request()->path()) }}">
         <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,300i,400,400i,700,700i,800,800i,900" rel="stylesheet">
         <link rel="stylesheet" href="{!! mix('css/app.css') !!}">
+
         @stack('css')
 
         @if ($livewire)
