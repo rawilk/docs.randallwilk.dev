001da98522f38d6e98c16db7d769c111e47a4798
Randall Wilk <wilk.randall@gmail.com>
2020-08-14 15:26:06 -0500

    Add sitemap

---
+++ app/Console/Commands/GenerateSitemap.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Spatie\Sitemap\SitemapGenerator;
+
+class GenerateSitemap extends Command
+{
+    protected $signature = 'sitemap:generate';
+
+    protected $description = 'Generate the sitemap.';
+
+    public function handle(): void
+    {
+        SitemapGenerator::create(config('app.url'))
+            ->writeToFile(public_path('sitemap.xml'));
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\GenerateSitemap;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -9,6 +10,7 @@ class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule): void
     {
+        $schedule->command(GenerateSitemap::class)->daily();
     }
 
     protected function commands(): void
