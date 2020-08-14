0cc47893524c0923af6678ae1c8cd1208a30b830
Randall Wilk <wilk.randall@gmail.com>
2020-08-14 10:51:12 -0500

    Add settings base redirect

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 // Package redirects to their "master" branches
 Route::redirect('laravel-app-key-rotator', '/laravel-app-key-rotator/v1/introduction');
 Route::redirect('laravel-printing', '/laravel-printing/v1/introduction');
+Route::redirect('laravel-settings', '/laravel-settings/v1/introduction');
 
 Route::get('{package}/{version}/{doc}', DocsController::class)
     ->where('doc', '.*')
